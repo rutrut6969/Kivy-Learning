a35f8ff491599e814b1424528a0f1cbdf85afd0c
rutrut6969 <rutledgeisaac6969@gmail.com>
2019-07-02 18:41:06 -0400

    Nav

--- Kivy-Lessons/Navigation/main.py
+++ Kivy-Lessons/Navigation/main.py
@@ -2,6 +2,7 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
 
 class MainWindow(Screen):
     pass
@@ -9,14 +10,15 @@ class MainWindow(Screen):
 class SecondWindow(Screen):
     pass
 
-class WindowsManager(ScreenManager):
+class WindowManager(ScreenManager):
     pass
 
-    
+
 
 
 
 kv = Builder.load_file("main.kv")
+passwd = ObjectProperty(None)
 
 
 
